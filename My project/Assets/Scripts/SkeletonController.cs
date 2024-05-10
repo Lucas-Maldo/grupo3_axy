ac26bc82c26a54f7c1fdd3bb902c372f08d06ef8
mjanas1 <mjanas@miuandes.cl>
2024-05-10 11:36:50 -0400

    DeltaTime on every movement or spawn

--- My project/Assets/Scripts/SkeletonController.cs
+++ My project/Assets/Scripts/SkeletonController.cs
@@ -8,33 +8,28 @@ public class RandomSpawner : MonoBehaviour
     GameObject[] arrows;
     GameObject skeleton;
     private float interval = 1f;
-    private float startTime;
     private float elapsedTime;
 
     void Start(){
-        startTime = Time.time;
         skeleton = GameObject.FindGameObjectsWithTag("Skeleton")[0];
     }
 
     void Update(){
-        elapsedTime = Time.time - startTime;
-    }
-
-    void FixedUpdate()
-    {
-        ArrowSpawn();
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime >= interval){
+            elapsedTime = 0f;
+            ArrowSpawn();
+        }
         ArrowDestroy();
     }
 
     void ArrowSpawn(){
-         if(elapsedTime >= interval){
-            startTime = Time.time;
-            Vector3 randomPosition = 3 * new Vector3(Random.Range(-3f, 4f), Random.Range(-3f, 4f), 0).normalized;
-            Vector2 randomSpawnPosition = skeleton.transform.position + randomPosition;
-            Debug.Log(randomPosition);
-            Instantiate(arrow, randomSpawnPosition, Quaternion.identity);
-        }
+        Vector3 randomPosition = 3 * new Vector3(Random.Range(-3f, 4f), Random.Range(-3f, 4f), 0).normalized;
+        Vector2 randomSpawnPosition = skeleton.transform.position + randomPosition;
+        Debug.Log(randomPosition);
+        Instantiate(arrow, randomSpawnPosition, Quaternion.identity);
     }
+
     void ArrowDestroy(){
         arrows = GameObject.FindGameObjectsWithTag("Arrow");
         if(arrows.Length > 1){
